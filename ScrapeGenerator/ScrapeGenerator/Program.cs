d755ca6b4ada3ed3d5d39beb624b02dba7768321
Franz-Filip <franzfilip.schoerghuber@gmail.com>
2023-01-10 10:08:09 +0100

    wip

--- ScrapeGenerator/ScrapeGenerator/Program.cs
+++ ScrapeGenerator/ScrapeGenerator/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Model;
+using System.Text.Json;
+
+internal class Program {
+    private static void Main(string[] args) {
+        Console.WriteLine("Hello, World!");
+        string json = "{\r\n  \"ClassName\": \"MyClass\",\r\n  \"Properties\": [\r\n    {\"Name\": \"Property1\", \"Type\": \"string\"},\r\n    {\"Name\": \"Property2\", \"Type\": \"int\"}\r\n  ],\r\n  \"Actions\": [\r\n    {\r\n      \"Kind\": \"NAVIGATE\",\r\n      \"URL\": \"http://www.example.com\",\r\n      \"PropertyPath\": \"\",\r\n      \"SubAction\": null\r\n    }\r\n  ]\r\n}";
+
+        Input input = JsonSerializer.Deserialize<Input>(json);
+    }
+}
